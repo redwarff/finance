0ac2bd9b6846ff9cab17ae3125f42e1656bf47ae
red <dominik.sm@seznam.cz>
2017-08-02 00:46:47 +0200

    fix port for heroku

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname + '/public/index.html'));
+	res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT || 3000, function () {
+	console.log(process.env.PORT ? 'Example app listening!' : 'Example app listening on port 3000!')
 })
