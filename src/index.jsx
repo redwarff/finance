013ad73cceec1bd51fcad6bbd3223b80e733214a
red <dominik.sm@seznam.cz>
2017-08-02 16:08:06 +0200

    test

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 
-import GuestStore from './stores/GuestStore'
 import routes from './routes'
+import GuestStore from './stores/GuestStore'
 
 const initialState = window.initialState || {
 	guests:[]
